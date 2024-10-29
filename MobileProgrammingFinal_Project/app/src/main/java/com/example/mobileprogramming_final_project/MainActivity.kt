8d4a4f08ec68e06d01954fa305bc393f13e2f1db
LisbethEch <lisbethe@uark.edu>
2024-10-29 13:40:20 -0500

    Add files via upload

---
+++ MobileProgrammingFinal_Project/app/src/main/java/com/example/mobileprogramming_final_project/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.mobileprogramming_final_project
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.tabs.TabItem
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var tabAction: TabItem
+    private lateinit var tabEvent: TabItem
+    private lateinit var tabConnect: TabItem
+    private lateinit var tabProfile: TabItem
+    private lateinit var tabLayout : TabLayout
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
